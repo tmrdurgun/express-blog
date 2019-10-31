d711287b712dc8ddd340a25838fcda33ccf4a6b6
Tamer Durgun <tmr.durgun@gmail.com>
2019-10-31 16:36:58 +0300

    remove unnecessary fields from comments & post

--- routes/post/all.js
+++ routes/post/all.js
@@ -4,22 +4,42 @@ module.exports = async (req, res) => {
     postModel.aggregate([
         {
             $lookup: {
-                from: 'comments', 
-                localField: '_id', 
-                foreignField: 'post', 
-                as: 'comments' 
-            },
-          }
-    ]).then( data => {
+                from: 'comments',
+                let: { id: '$_id'},
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: ['$post', '$$id']
+                            }
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            __v: 0
+                        }
+                    }
+                ],
+                as: 'comments',
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                __v: 0
+            }
+        }
+    ]).then(data => {
         res.send({
             status: 1,
             data: data
         })
     })
-    .catch(err => {
-        res.send({
-            status: 0,
-            message: err.message
-        })
-    });
+        .catch(err => {
+            res.send({
+                status: 0,
+                message: err.message
+            })
+        });
 }
